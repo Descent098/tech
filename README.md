f017dc6eb28ccef4e550a147691f3894db6dca62
Kieran Wood <kieran@canadiancoding.ca>
2023-12-10 01:21:44 -0700

    Added initial build

--- README.md
+++
@@ -1,8 +0,0 @@
-# Astro Redesign
-
-An Astro Redesign of my personal site to start learning the framework. Based on [Astrofy](https://astro.build/themes/details/astrofy/).
-
-To test:
-
-1. Install dependencies `npm install`
-2. Run `npm run dev`
--- src/config.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Kieran Wood';
-export const SITE_DESCRIPTION = 'I\'m a computer science major, with a minor in philosophy. I love to do open source work, especially in scripting, automation, web development, API\'s, CLI\'s and dev ops!';
