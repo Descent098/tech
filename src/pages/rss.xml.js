eafd12043da8dacf753b5b1c48c3d2410b0839a5
Kieran Wood <kieran@canadiancoding.ca>
2026-03-17 11:31:30 -0600

    fixed RSS feed generation (capitalized GET)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,12 +2,12 @@ import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 
-export async function get(context) {
+export async function GET(context) {
   const blog = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: import.meta.env.SITE,
+    site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
